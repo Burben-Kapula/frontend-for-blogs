d675113727e0350f003f9e8242b507f75bdb1b2a
Burben <burbenburbenov@gmail.com>
2025-11-28 11:49:56 +0200

    add buttom and form

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,27 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import './css/LoginForm.css'
 
 
 
 
 function LoginForm(){
+ const [action, setAction] = useState("Login")
+ 
+console.log(action)
+
+
+
 
   return (
     <>
       <div className="container">
         <div className="header">
-          <div className="text">Sing up</div>
+          <div className="text">{action}</div>
           <div className="underline"></div>
         </div>
         <div className="inputs">
           <div className="input">
-            <input type="text" />
+            <input type="text" placeholder="Enter name: "/>
           </div>
           <div className="input">
-            <input type="email" />
+            <input type="email" placeholder="Enter email: "/>
           </div>
           <div className="input">
-            <input type="password" />
+            <input type="password" placeholder="Enter password: "/>
+          </div>
+            <button className="forgot-password"><span>Forgot password</span></button>
+
+          <div className="submit-container">
+            <button className={action === "Login"?"submit gray":"submit"} onClick={()=>{setAction('Sing up')}}><span>Sing Up</span></button>
+            <button className={action === "Sing Up"?"submit gray":"submit"} onClick={()=>{setAction('Login')}}><span>Login</span></button>
           </div>
         </div>
       </div>
@@ -29,7 +41,6 @@ function LoginForm(){
   );
 }
 export default LoginForm
-
 
 
 
