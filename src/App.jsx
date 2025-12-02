00068c469d557f9cbe6a5aa78da5ebcbfdc1522d
Burben <burbenburbenov@gmail.com>
2025-12-02 09:12:38 +0200

    another fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
+import React from "react"
 import { Routes, Route } from "react-router-dom"
 import LoginForm from "./components/LoginForm"
 import SignUpForm from "./components/SingUpForm"
 import Home from "./components/Home"
-import React from "react"
+
 function App() {
   return (
     <Routes>
+      {/* стартова сторінка = логін */}
       <Route path="/" element={<LoginForm />} />
       <Route path="/signup" element={<SignUpForm />} />
       <Route path="/home" element={<Home />} />
