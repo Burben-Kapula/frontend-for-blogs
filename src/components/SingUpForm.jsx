d3f55fef14486582856988dc39b2c309a7b110f2
Burben <burbenburbenov@gmail.com>
2025-12-02 13:21:26 +0200

    2e1

--- src/components/SingUpForm.jsx
+++ src/components/SingUpForm.jsx
@@ -15,8 +15,8 @@ function SingUpForm() {
   console.log('SIGNUP DATA:', { name, email, password })
   try {
     const res = await axios.post(
-    "https://beckend-for-blogs.onrender.com/api/auth/singup",
-      {
+  "https://beckend-for-blogs.onrender.com/api/auth/register",
+        {
         username,
         email,
         password,
